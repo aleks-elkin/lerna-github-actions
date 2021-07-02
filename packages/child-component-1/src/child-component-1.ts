9c3a39a8518cb1bc3e98bbd4c90dc25ac6762aa3
Aleksandr Elkin <aleksandr.elkin@tradeshift.com>
2021-07-02 17:54:43 +0200

    feat: cleanup child 1 component

--- packages/child-component-1/src/child-component-1.ts
+++ packages/child-component-1/src/child-component-1.ts
@@ -3,6 +3,6 @@ export class ChildComponent1 {
 	public id: string;
 
 	constructor(id: string) {
-		this.id = "Changed again and again Child_1_" + id;
+		this.id = "Changed again Child_1_" + id;
 	}
 }
