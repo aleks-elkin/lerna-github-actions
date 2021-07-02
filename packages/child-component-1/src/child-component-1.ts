287ef3a3ed383ec55cd94d52b1ed32a8706b64ab
Aleksandr Elkin <aleksandr.elkin@tradeshift.com>
2021-07-02 13:39:55 +0200

    feat: changed child component 1

--- packages/child-component-1/src/child-component-1.ts
+++ packages/child-component-1/src/child-component-1.ts
@@ -3,6 +3,6 @@ export class ChildComponent1 {
 	public id: string;
 
 	constructor(id: string) {
-		this.id = "changed again and again child_1_" + id;
+		this.id = "Changed again and again child_1_" + id;
 	}
 }
