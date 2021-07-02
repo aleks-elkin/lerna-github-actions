543a2833157537f9c3ca835704e73c39f39f86a2
Aleksandr Elkin <aleksandr.elkin@tradeshift.com>
2021-07-02 14:07:26 +0200

    feat: added a new child 3 component

---
+++ packages/child-component-3/README.md
@@ -0,0 +1,11 @@
+# `child-component-3`
+
+> TODO: description
+
+## Usage
+
+```
+const childComponent3 = require('child-component-3');
+
+// TODO: DEMONSTRATE API
+```
--- packages/main-app-component/src/main-app-component.ts
+++ packages/main-app-component/src/main-app-component.ts
@@ -1,17 +1,20 @@
 import { ChildComponent1 } from "@aleks-elkin/test-lerna-repo.child-component-1";
 import { ChildComponent2 } from "@aleks-elkin/test-lerna-repo.child-component-2";
+import { ChildComponent3 } from "@aleks-elkin/test-lerna-repo.child-component-3";
 export class MainComponent {
 
 	private internalId = "test_monorepo";
 	private childComponent1: ChildComponent1;
 	private childComponent2: ChildComponent2;
+	private childComponent3: ChildComponent3;
 
 	public get id() {
-		return `${this.childComponent1.id}_+_${this.childComponent2}`;
+		return `${this.childComponent1.id}_+_${this.childComponent2}_+_${this.childComponent3}`;
 	}
 	
 	constructor() {
 		this.childComponent2 = new ChildComponent2(this.internalId);
 		this.childComponent1 = new ChildComponent1(this.internalId);
+		this.childComponent3 = new ChildComponent3(this.internalId);
 	}
 }
