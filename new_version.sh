99ae5a01858633840c47acfe5c40e09dcf0695d7
Aleksandr Elkin <aleksandr.elkin@tradeshift.com>
2021-07-20 16:16:56 +0200

    ci: add tag in script

--- new_version.sh
+++ new_version.sh
@@ -6,10 +6,12 @@ latest_release="release/$(git describe --abbrev=0)"
 
 if ($latest_release == $version)
 then
-	echo 'No new version. Exit.'
+	echo "No new version. Exit."
 else
 	branch="release/$version"
 	git checkout -b $branch
-	git add . && git commit -m "chore(release): $version"
+	git add .
+	git commit -m "chore(release): $version"
+	git tag $version
 	git push origin $branch
 fi
