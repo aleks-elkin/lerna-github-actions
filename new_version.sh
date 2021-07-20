5867cb1711af8e810c2a3be415eec43f63a1472b
Aleksandr Elkin <aleksandr.elkin@tradeshift.com>
2021-07-20 16:51:41 +0200

    ci: fix script

--- new_version.sh
+++ new_version.sh
@@ -9,10 +9,17 @@ if [ $latest_release = $version ]
 then
 	echo "No new version. Exit."
 else
+	# create a new branch
 	branch="release/$version"
 	git checkout -b $branch
+
+	# commit a changelog and a new version
 	git add .
 	git commit -m "chore(release): $version"
+
+	# push the new branch to origin
 	git push --set-upstream origin $branch
-	git push origin --tags
+
+	# push the new tag
+	git push origin $version
 fi
